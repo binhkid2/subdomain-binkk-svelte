2fd16eb7bf08a2e9d1b094d7e01b8923c47d1bcf
binhkid2 <binhkid2@gmail.com>
2023-08-13 23:52:14 +0700

    lol

--- src/hooks.server.ts
+++ src/hooks.server.ts
@@ -1,11 +1,32 @@
-
-
 export async function handle({ event, resolve }) {
-  // if route matches "/banana" return banana
+  // if hostname starts with "hello."
   if (event.url.hostname.startsWith('hello.')) {
-    return fetch('http://localhost:5173/hello' || "https://subdomain-binkk-svelte.vercel.app/hello" || "https://bisite.click/hello")
+    try {
+      // Try to fetch the desired URL
+      const response = await fetch('http://localhost:5173/hello');
+      return response;
+    } catch (error) {
+      // If fetch fails, try the fallback URLs
+      const fallbackUrls = [
+        
+        "https://bisite.click/hello"
+      ];
+
+      for (const url of fallbackUrls) {
+        try {
+          const response = await fetch(url);
+          return response;
+        } catch (error) {
+          // Fetch failed, continue to the next fallback URL
+          continue;
+        }
+      }
+
+      // All fetch attempts failed, return an error response
+      return new Response('Failed to fetch any URL', { status: 500 });
+    }
   }
 
   // otherwise use the default behavior
-  return resolve(event)
+  return resolve(event);
 }
