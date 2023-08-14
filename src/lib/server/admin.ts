03d60ebd62b3f5995197d6b12d39216b1b07552f
binhkid2 <binhkid2@gmail.com>
2023-08-14 17:12:53 +0700

    ss

--- src/lib/server/admin.ts
+++ src/lib/server/admin.ts
@@ -1,20 +1,22 @@
 import { getAuth } from 'firebase-admin/auth';
 import { getFirestore } from 'firebase-admin/firestore';
 import { FB_CLIENT_EMAIL, FB_PRIVATE_KEY, FB_PROJECT_ID } from '$env/static/private'
-import  * as firebaseAdmin  from 'firebase-admin';
-
-import firebaseAccountCredentials from './service-account.json'
-const serviceAccount = firebaseAccountCredentials as firebaseAdmin.ServiceAccount
+import pkg from 'firebase-admin';
 
 try {
-  firebaseAdmin.initializeApp({
-    credential: firebaseAdmin.credential.cert(serviceAccount)
+  pkg.initializeApp({
+    credential: pkg.credential.cert({
+      projectId: FB_PROJECT_ID,
+      clientEmail: FB_CLIENT_EMAIL,
+      privateKey: FB_PRIVATE_KEY,
+    }),
   });
 } catch (err: any) {
   if (!/already exists/u.test(err.message)) {
     console.error('Firebase Admin Error: ', err.stack)
   }
 }
+
 
 export const adminDB = getFirestore();
 export const adminAuth = getAuth();
